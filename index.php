afadfbd8adba6a5630b9a11ba4d130bf269910ef
Anonymous44444444 <arjohn.potot@dssc.edu.ph>
2026-04-22 15:23:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-// simple router
-$request = $_SERVER['REQUEST_URI'];
+// remove query string (?utm=... etc.)
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($request === '/' || $request === '/index.php') {
+// normalize
+$request = rtrim($request, '/');
+
+if ($request === '' || $request === '/index.php') {
     include 'Login.php';
 } else {
     $file = ltrim($request, '/');
+
     if (file_exists($file)) {
         include $file;
     } else {
